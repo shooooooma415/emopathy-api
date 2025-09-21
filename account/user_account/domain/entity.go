24abc5a85de87b4205dcba3336fb5ef58cdcae57
shooooooma415 <shoma20040415@icloud.com>
2025-09-21 18:45:44 +0900

    feat: entityを定義

---
+++ account/user_account/domain/entity.go
@@ -0,0 +1,21 @@
+package core
+
+import "emopathy-api/shared/common_vo"
+
+type RootEntity struct {
+	id          ID
+	fcmToken    fcmToken
+	accessToken accessToken
+	createdAt   createdAt
+	updatedAt   updatedAt
+}
+
+func NewRootEntity(fcmToken fcmToken, accessToken accessToken, clock common_vo.Clock) *RootEntity {
+	return &RootEntity{
+		id:          RandomID(),
+		fcmToken:    fcmToken,
+		accessToken: accessToken,
+		createdAt:   NewCreatedAt(clock.Now()),
+		updatedAt:   NewUpdatedAt(clock.Now()),
+	}
+}
